6af62fbae0621c8bf01a2e1c1450a92baa98a88f
AMANGUJJAR <ag3734359@gmail.com>
2024-07-16 23:48:13 +0500

    Typescript-series

---
+++ class 01/class-01.js
@@ -0,0 +1,9 @@
+"use strict";
+var a;
+a = 10;
+console.log(a);
+let b;
+b = 15;
+console.log(b);
+const c = 20;
+console.log(c);
---
+++ class 01/class-01.ts
@@ -0,0 +1,14 @@
+
+var a ;
+a =10 ;
+
+console.log(a);
+
+
+let b ;
+b = 15;
+console.log(b);
+
+const c = 20 ;
+console.log(c);
+
