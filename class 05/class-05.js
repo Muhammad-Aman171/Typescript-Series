c3b576d54a95ae3d5788215c89966f4cedcd26a3
AMANGUJJAR <ag3734359@gmail.com>
2024-07-22 00:45:02 +0500

    class-05

---
+++ class 05/class-05.js
@@ -0,0 +1,10 @@
+"use strict";
+// function a(b:number,c:number) {
+//    let d = b+c
+//    return d
+// }
+let a = (b, c) => num1 + num2;
+let num1 = 16;
+let num2 = 15;
+let result = a(num1, num2);
+console.log(result);
---
+++ class 05/class-05.ts
@@ -0,0 +1,13 @@
+// function a(b:number,c:number) {
+//    let d = b+c
+//    return d
+// }
+
+let a = (b:number,c:number)=>num1+ num2
+
+
+let num1: number = 16;
+let num2: number = 15;
+let result = a(num1,num2)
+
+console.log(result);
