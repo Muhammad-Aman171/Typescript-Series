f814d96a4c95133db2b6d0bf2d3d7dc502da4e62
Muhammad-Aman171 <ag3734359@gmail.com>
2024-10-02 10:50:42 +0500

    class-07

--- class 07/script.js
+++ class 07/script.js
@@ -1,2 +1,24 @@
 "use strict";
-let a = 10;
+// let inputarr = +(prompt("enter your array") || "0" );
+// let arr:number[] = [inputarr];
+// //  arr = inputarr[]
+// // let arr: string[] = ["Aman", "zain", "abubakar", "fareed", "sabir"];
+// // let arr: number[] = [32,45,76];
+// let newArr = arr.map((name) => name ** 2);
+// console.log(arr);
+// console.log(newArr);
+// function getArrayFromUser(): string[] {
+//     const userInput: string | null = prompt("Enter elements separated by commas:");
+//     if (userInput) {
+//       // User input ko split karke array mein convert karna
+//       const userArray: string[] = userInput.split(',').map(item => item.trim());
+//       return userArray;
+//     } else {
+//       return [];
+//     }
+//   }
+//   const userArray = getArrayFromUser();
+//   console.log(userArray);
+let arr = [1, 2, 2, 2, 3, 4, 65, 3, 4, 6, 5, 4];
+let newArr = arr.filter((num, index) => arr.indexOf(num) === index);
+console.log(newArr);
